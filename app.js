71bec933ed0030f663a429b7c3994e2e3985697d
Stephanie Leazer <saleazer82@gmail.com>
2021-03-19 19:31:55 -0500

    Last planned update

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
+const chalk = require('chalk')
 
-geocode('hawaii', (error, data) => {
-    // console.log('Error', error)
-    console.log('data', data)
-    forecast(data.latitude, data.longitude, (error, data) => {
-        // console.log('Error', error)
-        console.log('Data', data)
-      })
-})
+const location = process.argv[2]
 
+if(!location) {
+    console.log(chalk.red.bold('Please provide location..'))
+} else {
+    geocode(location, (error, {latitude, longitude} = {}) => {
+        if (error) {
+            return console.log(error)
+        }
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+
+            console.log(forecastData)
+        })
+    })
+}
--- utils/forecast.js
+++ utils/forecast.js
@@ -2,20 +2,20 @@ const request = require('request')
 const chalk = require('chalk')
 
 const forecast = (latitude, longitude, callback) => {
-    const urlWeather = "http://api.weatherstack.com/current?access_key=a881442ca3b9f7e89ca6149054c1fbc1&query="+latitude+","+longitude+"&units=f"
-    request({ url: urlWeather, json: true }, (error, response) => {
+    const url = "http://api.weatherstack.com/current?access_key=a881442ca3b9f7e89ca6149054c1fbc1&query="+latitude+","+longitude+"&units=f"
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback("Unable to connect to weather service", undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
                 callback("Unable to find location", undefined)
 
         } else {
-            callback(undefined, chalk.magentaBright(response.body.location.name + ", " + response.body.location.region))
-            callback(undefined, "It is currently " + chalk.yellow.bold(response.body.current.temperature) + " degrees F")
-            callback(undefined, "The windchill makes it feel " + chalk.green.bold(response.body.current.feelslike) + " degrees F")
-            callback(undefined, "The current windspeed is " + chalk.blueBright(response.body.current.wind_speed + " " + response.body.current.wind_dir))
-            callback(undefined, "The current time is " + chalk.cyanBright(response.body.location.localtime))
-            callback(undefined, "The wind pressure is " + chalk.red.bold(response.body.current.pressure))   
+            callback(undefined, chalk.magentaBright(body.location.name + ", " + body.location.region))
+            callback(undefined, "It is currently " + chalk.yellow.bold(body.current.temperature) + " degrees F")
+            callback(undefined, "The windchill makes it feel " + chalk.green.bold(body.current.feelslike) + " degrees F")
+            callback(undefined, "The current windspeed is " + chalk.blueBright(body.current.wind_speed + " " + body.current.wind_dir))
+            callback(undefined, "The current time is " + chalk.cyanBright(body.location.localtime))
+            callback(undefined, "The wind pressure is " + chalk.red.bold(body.current.pressure))   
         }
     })
 }
